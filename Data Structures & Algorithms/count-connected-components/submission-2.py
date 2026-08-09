35d41f1bbdbefac92575e15befb5b22fde1ce330
Dsavvi180 <127865498+Dsavvi180@users.noreply.github.com>
2026-08-09 17:56:04 +0100

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+from collections import deque
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        nodes = defaultdict(list)
+        for A,B in edges:
+            nodes[A].append(B)
+            nodes[B].append(A)
+        visited = set()
+        count = 0
+        for node, neighbors in nodes.items():
+            if node not in visited:
+                count+=1
+                queue = deque([node])
+                visited.add(node)
+                while queue:
+                    nextNode = queue.popleft()
+                    for nd in nodes.get(nextNode):
+                        if nd not in visited:
+                            queue.append(nd)
+                            visited.add(nd)
+        return count + n - len(nodes)
+
+
+
+        
+        
+        

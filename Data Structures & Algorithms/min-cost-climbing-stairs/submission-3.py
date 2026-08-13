7508b02d68cb1ed8ceaa8777629a1c02474b8376
Dsavvi180 <127865498+Dsavvi180@users.noreply.github.com>
2026-08-13 17:43:49 +0100

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.py
@@ -0,0 +1,21 @@
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        self.cache = [-1] * (n)
+
+        def step(position):
+
+            if position >= n:
+                return 0
+
+            if self.cache[position] == -1:
+                self.cache[position] = min(step(position+1), step(position+2)) + cost[position]
+            
+            return self.cache[position]
+        
+        step(0)
+        step(1)
+       
+        return min(self.cache[0], self.cache[1])
+        

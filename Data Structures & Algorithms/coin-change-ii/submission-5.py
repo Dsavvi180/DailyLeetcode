aed942e64bed1fc9ef0210ab33dac25844fbecf1
Dsavvi180 <127865498+Dsavvi180@users.noreply.github.com>
2026-09-02 00:13:16 +0100

    Add: coin-change-ii - submission-5

---
+++ Data Structures & Algorithms/coin-change-ii/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        if amount == 0:
+            return 1
+
+        memo = {}
+
+        def dfs(total, position):
+
+            if total > amount or position == len(coins):
+                return 0
+
+            if total == amount:
+                return 1
+
+            if (total, position) in memo:
+                return memo[(total, position)]
+    
+            sameCoin = dfs(total+coins[position], position)
+            nextCoin = dfs(total, position+1)
+            
+            memo[(total, position)] = sameCoin + nextCoin
+            return sameCoin + nextCoin
+        
+        return dfs(0,0)
